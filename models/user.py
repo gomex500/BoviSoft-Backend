95aaaa54e48d0c5434c0e765236d2578b80edd37
gomex500 <gomezfreddy886@gmail.com>
2024-08-13 22:39:57 -0600

    se mejoraron los datos del usuario

--- models/user.py
+++ models/user.py
@@ -5,7 +5,7 @@ class UserModel:
     def __init__(self, data):
         self.nombre = data.get('nombre', '')
         self.apellido = data.get('apellido', '')
-        self.edad = data.get('edad', 0)
+        self.edad = data.get('fecha_nacimiento', '')
         self.email = data.get('email', '')
         self.password = data.get('password', '')
         self.create_at = data.get('create_at', datetime.now())
