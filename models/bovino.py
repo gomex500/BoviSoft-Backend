150cc4622f4e181ae68675daeb502887d87f9b9e
Junior <jhurtado@plannerapp.net>
2024-10-03 23:58:54 -0600

    se edito el modelo de los bovinos

--- models/bovino.py
+++ models/bovino.py
@@ -1,8 +1,19 @@
 from datetime import datetime
+from bson import ObjectId
 
-##modelo de objeto Bovino
 class BovinoModel:
     def __init__(self, data):
+        self.fincaId = ObjectId(data.get('fincaId')) 
         self.nombre = data.get('nombre', '')
+        self.raza = data.get('raza', '')
+        self.edad = data.get('edad', '')
+        self.peso = data.get('peso', '')
+        self.consecutivo = data.get('consecutivo', None) 
+        self.codigo = data.get('codigo', '')
+        self.fechaNacimiento = data.get('fechaNacimiento', None)  # Puede ser un string o un datetime
+        self.genero = data.get('genero', '')  # Ej: "macho", "hembra"
+        self.tipo = data.get('tipo', '')  # Ej: "carne", "leche", "mixto"
+        self.estadoSalud = data.get('estadoSalud', '')  # Ej: "sano", "enfermo"
+        self.fechaRegistro = data.get('fechaRegistro', datetime.now())
         self.create_at = data.get('create_at', datetime.now())
         self.update_at = data.get('update_at', datetime.now())
