53c0767eb387375717ead522fb39c23838455d1f
gomex500 <gomezfreddy886@gmail.com>
2024-10-03 23:58:04 -0600

    se modifico el modelo de alertas

--- models/alertas.py
+++ models/alertas.py
@@ -5,10 +5,16 @@ class AlertasModel:
     def __init__(self, data):
         self.idUsuario = data.get('idUsuario', '')
         self.tipoAlerta = data.get('tipoAlerta', '') ##Ej: "vacunación", "enfermedad", "producción baja"
-        self.mensaje = data.get('mensaje', '')
-        self.idFinca = data.get('idFinca', '')
-        self.idBovino = data.get('idBovino', '')
+        self.detalles = data.get('detalles', {})
         self.fechaAlerta = data.get('fechaAlerta', '')
         self.leido = data.get('leido', '')
         self.create_at = data.get('create_at', datetime.now())
         self.update_at = data.get('update_at', datetime.now())
+
+
+#ejemplo de detalles
+### "detalles": {
+#      "idFinca": "ObjectId",
+#      "idBovino": "ObjectId",
+#      "mensaje": "string"
+#    }
