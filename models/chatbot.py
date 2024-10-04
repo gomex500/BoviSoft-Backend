20cf27605ad35fb5cd1883fc8a54a1a303403bd8
gomex500 <gomezfreddy886@gmail.com>
2024-10-03 23:49:34 -0600

    se creo el modelo chatbot

---
+++ models/chatbot.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+##modelo de objeto Chabot
+class ChatBotModel:
+    def __init__(self, data):
+        self.idUsuario = data.get('idUsuario', '')
+        self.preguntaUsuario = data.get('preguntaUsuario', '')
+        self.respuestaChatbot = data.get('respuestaChatbot', '')
+        self.fecha = data.get('fecha', '')
+        self.categoria = data.get('categoria', '') ## Ej: "salud", "producción", "recomendaciones"
+        self.create_at = data.get('create_at', datetime.now())
+        self.update_at = data.get('update_at', datetime.now())
